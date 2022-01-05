56a61969454590fd56c55840a1e8a4bc6072b96b
Gn0lee <jinho605@gmail.com>
2022-01-05 07:42:31 +0000

    project2 thread edit1

--- threads/thread.c
+++ threads/thread.c
@@ -219,9 +219,6 @@ thread_create (const char *name, int priority,
     t->fdTable[0] = 1;
     t->fdTable[1] = 2;
 
-
-
-
     /* Call the kernel_thread if it scheduled.
      * Note) rdi is 1st argument, and rsi is 2nd argument. */
     t->tf.rip = (uintptr_t) kernel_thread;
